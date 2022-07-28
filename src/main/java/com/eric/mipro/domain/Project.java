4346682576de2006999ed99c4527fed3cf85f735
rezae <reza.eftekharzadeh@apotek1.no>
2022-07-28 22:44:08 +0200

    feature/mp-5_validation for Project domain model

--- src/main/java/com/eric/mipro/domain/Project.java
+++ src/main/java/com/eric/mipro/domain/Project.java
@@ -1,14 +1,18 @@
 package com.eric.mipro.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -20,22 +24,32 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Project name is required")
     private String projectName;
+
+    @NotBlank(message = "identifier is required")
+    @Size(min = 4, max = 5, message = "Not allowed to use more than 4 and less than 5 char")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
-    private  String description;
+    private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     @PrePersist
-    protected void onCreate(){
+    protected void onCreate() {
         this.created_At = new Date();
     }
 
     @PreUpdate
-    protected void onUpdate(){
+    protected void onUpdate() {
         this.updated_At = new Date();
     }
 
