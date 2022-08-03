2f162ace845537767abe6528e3eccb2fa8f47509
rezae <reza.eftekharzadeh@apotek1.no>
2022-08-03 14:30:58 +0200

    bugfix/mp-6 find by identifier

--- src/main/java/com/eric/mipro/advice/ApplicationExecptionHandler.java
+++ src/main/java/com/eric/mipro/advice/ApplicationExecptionHandler.java
@@ -1,10 +1,12 @@
 package com.eric.mipro.advice;
 
+import com.eric.mipro.advice.exception.ProjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,4 +24,11 @@ public class ApplicationExecptionHandler {
                  .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
         return errors;
     }
+
+  /*  @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(RuntimeException.class)
+    public Map<String, String> handleNonExistProject(RuntimeException exception, WebRequest request) {
+        ProjectNotFoundException projectNotFoundException = new ProjectNotFoundException(exception.getMessage());
+
+    }*/
 }
--- src/main/java/com/eric/mipro/web/ProjectController.java
+++ src/main/java/com/eric/mipro/web/ProjectController.java
@@ -1,15 +1,19 @@
 package com.eric.mipro.web;
 
+import com.eric.mipro.advice.exception.ProjectNotFoundException;
 import com.eric.mipro.domain.Project;
 import com.eric.mipro.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/project")
@@ -21,11 +25,23 @@ public class ProjectController {
         this.projectService = projectService;
     }
 
-    @PostMapping("/")
+    @PostMapping("")
     public ResponseEntity<Project> createNewProject(@Valid @RequestBody Project.ProjectBuilder project) {
         project.projectName(project.getProjectName());
         project.projectIdentifier(project.getProjectIdentifier());
         Project newProject = project.build();
         return new ResponseEntity<>(projectService.saveOrUpdate(newProject), HttpStatus.CREATED);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findProjectByProjectId(@Valid @PathVariable Long id) throws ProjectNotFoundException {
+        return new ResponseEntity<>(projectService.findProjectById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/identifier/{identifier}")
+    public ResponseEntity<?> findProjectByProjectIdentifier(@Valid @PathVariable String identifier) throws ProjectNotFoundException {
+        return new ResponseEntity<>(projectService.findProjectByIdentifier(identifier), HttpStatus.OK);
+    }
+
+
 }
