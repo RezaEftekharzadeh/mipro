21f4cfdbca6664bd19b31a84d292b3730cd39aad
rezae <reza.eftekharzadeh@apotek1.no>
2022-08-03 15:40:18 +0200

    bugfix/mp-10 solve confilict

--- src/main/java/com/eric/mipro/web/ProjectController.java
+++ src/main/java/com/eric/mipro/web/ProjectController.java
@@ -5,7 +5,6 @@ import com.eric.mipro.domain.Project;
 import com.eric.mipro.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/project")
@@ -42,6 +40,11 @@ public class ProjectController {
     @GetMapping("/identifier/{identifier}")
     public ResponseEntity<?> findProjectByProjectIdentifier(@Valid @PathVariable String identifier) throws ProjectNotFoundException {
         return new ResponseEntity<>(projectService.findProjectByIdentifier(identifier), HttpStatus.OK);
+    }
+
+    @GetMapping("/allproject")
+    public Iterable<Project> findAllProjects(){
+        return projectService.findAllProject();
     }
 
 }
